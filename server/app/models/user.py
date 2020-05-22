b3ba5a3aa1735053666f10066d1f56b1194354a3
ernestlwt <ernestlimwoonteng@gmail.com>
2020-05-22 21:59:10 +0800

    added simple registration and login

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -1,8 +1,31 @@
 from app import db
 
-print("entered user")
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(120), index=True, unique=True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    @staticmethod
+    def create_user(username, password):
+        user = User(username, password)
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except:
+            return None
+        return user
+
+    @staticmethod
+    def login_user(username, password):
+        result = User.query.filter_by(username=username).first()
+        if result is None:
+            return None
+        if result.username == username and result.password == password:
+            return result
+        else:
+            return None
+        
--- server/app/routes/user_route.py
+++ server/app/routes/user_route.py
@@ -1,9 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import get_jwt_identity, create_access_token
 
 from app.models.user import User
 
 user_blueprint = Blueprint('user_blueprint', __name__)
 
-@user_blueprint.route('/login')
+@user_blueprint.route('/login', methods=['POST'])
 def index():
-    return "login"
+    username = request.values['username']
+    password = request.values['password']
+
+    if not username:
+        return 'No Username', 404
+    if not password:
+        return 'No Password', 404
+    
+    user = User.login_user(username=username, password=password)
+    if user is None:
+        return 'Login Failed', 400
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+
+@user_blueprint.route('/register', methods=['POST'])
+def register():
+    username = request.values['username']
+    password = request.values['password']
+
+    if not username:
+        return 'No Username', 404
+    if not password:
+        return 'No Password', 404
+    
+    user = User.create_user(username=username, password=password)
+    if user is None:
+        return 'Registration Failed', 400
+
+    return 'Registration Successful', 200
