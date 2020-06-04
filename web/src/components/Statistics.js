5bf926dbdb018546d527a1c3d7b43ce1e54fafa4
ernestlwt <ernestlimwoonteng@gmail.com>
2020-06-04 11:17:27 +0800

    added min max avg on statistic page

--- web/src/components/Statistics.js
+++ web/src/components/Statistics.js
@@ -3,18 +3,160 @@ import {
     Container,
     Row,
     Col,
-    Card
+    Form,
+    Button
 } from 'react-bootstrap';
-import '../css/Homepage.css';
 
 import {
-    get_latest_count
+    get_location,
+    get_statistics
 } from '../services/API';
 
+// eslint-disable-next-line
+Date.prototype.addHours = function(h){
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+}
+
+// eslint-disable-next-line
+Date.prototype.getInputString = function(){
+    let day = this.getDate();
+    let month = this.getMonth()+1;
+    let year = this.getFullYear();
+    let hour = this.getHours();
+    let minute = this.getMinutes();
+    if(day < 10){
+        day = "0"+ day;
+    }
+    if(month < 10){
+        month = "0"+ (month);
+    }
+    if(hour < 10){
+        hour = "0"+ (hour);
+    }
+    if(minute < 10){
+        minute = "0"+ (minute);
+    }
+    return year + "-" + month + "-" + day + "T" + hour + ":" + minute;
+}
+
 function Statistics() {
+    let [ locationList, setLocationList ] = React.useState([]);
+
+    let [ startDate, setStartDate ] = React.useState("");
+    let [ endDate, setEndDate ] = React.useState("");
+    let [ location, setLocation] = React.useState("");
+
+    let [ recordList , setRecordList ] = React.useState([]);
+    let [ max , setMax ] = React.useState(0);
+    let [ min , setMin ] = React.useState(0);
+    let [ avg , setAvg ] = React.useState(0);
+
+    React.useEffect(() => {
+        async function fetchData(){
+            let location_response = await get_location();
+            setLocationList(location_response.data);
+        }
+        fetchData();
+        let date_start = new Date();
+        let date_end = new Date();
+        date_start = date_start.addHours(-1);
+        setStartDate(date_start.getInputString());
+        setEndDate(date_end.getInputString());
+    }, []);
+
+    React.useEffect(() => {
+        setLocation(locationList[0]);
+    },[locationList]);
+
+    const onClickStatistics = async () => {
+        let statistic_response = await get_statistics(location.id, startDate, endDate);
+        setRecordList(statistic_response.data.stats);
+    }
+
+    React.useEffect(() => {
+        if(recordList === undefined || recordList.length === 0){
+            setMax("-");
+            setMin("-");
+            setAvg("-");
+            return;
+        }
+        let max = 0;
+        let min = 999;
+        let total = 0;
+        for(let i = 0; i < recordList.length; i++){
+            if(recordList[i].count > max){
+                max = recordList[i].count
+            }
+            if(recordList[i].count < min){
+                min = recordList[i].count
+            }
+            total = total + recordList[i].count
+        }
+        setMax(max);
+        setMin(min);
+        setAvg(total/recordList.length);
+    }, [recordList]);
+
+    React.useEffect(() => {
+        console.log(startDate);
+        console.log(endDate);
+    }, [startDate, endDate]);
+
     return(
         <Container>
-            <h1>Hello</h1>
+            <Row>
+                <Col sm="12" lg="3">
+                    <h1>Statistics</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="12" lg="3">
+                    <Form.Group>
+                        <Form.Label>Location:</Form.Label>
+                        <Form.Control as="select" value={location} onChange={(e) => {setLocation(e.target.value)}}>
+                            {
+                                locationList.map((l) => {
+                                    return (
+                            <option key={l.name + "_" + l.id} value={l.name}>{l.name}</option>
+                                    )
+                                })
+                            }
+                        </Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col sm="12" lg="3">
+                    <Form.Group>
+                        <Form.Label>Start Date:</Form.Label>
+                        <Form.Control type="datetime-local" step="60" value={startDate} onChange={(e) => {setStartDate(e.target.value)}}></Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col sm="12" lg="3">
+                    <Form.Group>
+                        <Form.Label>End Date:</Form.Label>
+                        <Form.Control type="datetime-local" step="60" value={endDate} onChange={(e) => {setEndDate(e.target.value)}}></Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col sm="12" lg="3" className="d-flex align-items-end flex-column">
+                    <Button className="mt-auto mb-3" onClick={() =>{onClickStatistics()}}>Get Statistics</Button>
+                </Col>
+            </Row>
+            <hr />
+            <Row>
+                
+            </Row>
+            <hr />
+            <Row>
+                <Col sm="12" lg="4">
+                    <h3>Max: {max}</h3>
+                </Col>
+                <Col sm="12" lg="4">
+                    <h3>Min: {min}</h3>
+                </Col>
+                <Col sm="12" lg="4">
+                    <h3>Avg: {avg}</h3>
+                </Col>
+            </Row>
         </Container>
     );
 }
--- web/src/services/API.js
+++ web/src/services/API.js
@@ -9,3 +9,11 @@ export function get_latest_count(){
 export function get_location(){
     return axios.get(API_SERVER+'/location');
 }
+
+export function get_statistics(location_id, start_time, end_time){
+    let data = new FormData();
+    data.append('location_id', location_id);
+    data.append('start_time', start_time);
+    data.append('end_time', end_time);
+    return axios.post(API_SERVER+'/location/statistics' ,data);
+}
