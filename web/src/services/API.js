83efea90b148cd32bc1a977213b1cb15d931ca01
ernestlwt <ernestlimwoonteng@gmail.com>
2020-06-08 12:26:58 +0800

    revert default api server back to localhost

--- web/src/services/API.js
+++ web/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_SERVER = (process.env.REACT_APP_API_SERVER !== undefined) ? (process.env.REACT_APP_API_SERVER) : ("http://localhost:5000");
-const API_SERVER = (process.env.REACT_APP_API_SERVER !== undefined) ? (process.env.REACT_APP_API_SERVER) : ("https://dc-api.ernestlwt.com");
+const API_SERVER = (process.env.REACT_APP_API_SERVER !== undefined) ? (process.env.REACT_APP_API_SERVER) : ("http://localhost:5000");
+// const API_SERVER = (process.env.REACT_APP_API_SERVER !== undefined) ? (process.env.REACT_APP_API_SERVER) : ("https://dc-api.ernestlwt.com");
 
 export function get_latest_count(){
     return axios.get(API_SERVER+'/location/get_latest_count');
