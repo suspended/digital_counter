03003303b122f17936c5ada64541dc968d463384
ernestlwt <ernestlimwoonteng@gmail.com>
2020-06-09 16:28:55 +0800

    Revert "fix possible bug on using cache"
    This reverts commit 2d6a4456cd7214fa07e29b598c14bc7e685bfd51.

--- server/app/models/counter.py
+++ server/app/models/counter.py
@@ -90,11 +90,11 @@ class Counter(db.Model):
 
     @classmethod
     def get_statistics(cls, location_id, start_time, end_time):
-        results = db.session.query(cls).options(FromCache(cache)).filter(
+        results = db.session.query(cls).filter(
             cls.location_id==location_id,
             cls.time >= start_time,
             cls.time <= end_time
-        ).all()
+        ).options(FromCache(cache)).all()
         return results
 
     @classmethod
